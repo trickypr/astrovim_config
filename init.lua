fa059282c746a662bf3d1976e95d5231d03d3e49
trickypr <trickypr@icloud.com>
2022-09-04 11:07:39 +1000

    Assorted config

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ local config = {
     pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
     skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_reload = false, -- automatically reload and sync packer after a successful update
+    auto_reload = true, -- automatically reload and sync packer after a successful update
     auto_quit = false, -- automatically quit the current session after a successful update
     -- remotes = { -- easily add new remotes to track
     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
@@ -44,6 +44,7 @@ local config = {
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   options = {
     opt = {
+      colorcolumn = "80",
       relativenumber = true, -- sets vim.opt.relativenumber
     },
     g = {
